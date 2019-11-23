dfb258850fe666abf3b308138bc6745ae4c6d261
jakobnissen <jakobnybonissen@gmail.com>
2019-11-23 13:32:07 +0100

    more changes

--- README.md
+++ README.md
@@ -3,33 +3,52 @@ Stack-allocated integer sets in Julia
 
 A small toy project to show the conciseness, abstraction and speed of Julia.
 
-This module implements one custom type - the `StackBitSet{M}`. A `StackBitSet{M}` is a set of integers in 0:(M-1) encoded in the 64 bits of this structs.
+This module implements one custom type - the `StackSet{M}`. A `StackSet{M}` is a set of integers in 0:(M-1) encoded in the lower M bits of a machine integer.
 
-In 139 lines, this module implements:
+In 121 lines, this module implements:
 
-* some basic methods: `copy`, `empty`, `isempty`, , custom iteration, `length`, `minimum`, `maximum`, `last`, `in`, `push`, `delete` and `pop`.
+* some basic methods: `copy`, `empty`, `isempty`, custom iteration, `length`, `minimum`, `maximum`, `last`, `in`, `push`, `delete` and `pop`.
 * some set-specific methods: `allunique`, `union`, `intersect`, `setdiff`, `complement` and `isdisjoint`.
 * As well as converters and constructors.
 
-A `StackBitSet` behaves exactly like an `AbstractSet`:
+A `StackSet` behaves exactly like you would expect an `AbstractSet` to do - other than that it is immutable:
 
 ```
-julia> a = StackBitSet([11, 51, 0, 27])
-StackBitSet{64}([0,11,27,51])
+julia> a = StackSet(2i+3 for i in 2:3:16)
+StackSet{64}([7,13,19,25,31])
 
-julia> length(union(a, a))
-4
+julia> 7 in a ? length(union(a, a)) : length(intersect(a, complement(a)))
+5
+```
+
+It is safe to use:
+```
+julia> pop(a, 6)
+ERROR: KeyError: key 6 not found
+Stacktrace:
+ [1] pop(::Main.SmallBitSet.StackSet{64}, ::Int64) at /home/jakni/Documents/scripts/play/SmallBitSet.jl/src/SmallBitSet.jl:100
+ [2] top-level scope at none:0
+
+julia> push(a, -1)
+ERROR: ArgumentError: StackSet{64} can only contain 0:63
+Stacktrace:
+ [1] throw_StackSet_digit_err(::Val{64}) at /home/jakni/Documents/scripts/play/SmallBitSet.jl/src/SmallBitSet.jl:21
+ [2] push(::Main.SmallBitSet.StackSet{64}, ::Int64) at /home/jakni/Documents/scripts/play/SmallBitSet.jl/src/SmallBitSet.jl:90
+ [3] top-level scope at none:0
 ```
 
 And is *extremely* efficiently implemented, with most set operations done in single clock cycles:
 
 ```
-julia> a, b = StackBitSet{11}([1, 5, 9, 0]), StackBitSet{41}([31, 21, 1, 0]);
+julia> a, b = StackSet{11}(1:3:12), StackSet{41}(4:7:40);
 
 julia> f(x, y) = length(setdiff(x, complement(y)));
 
+julia> f(a, b)
+1
+
 julia> code_native(f, Tuple{typeof(a), typeof(b)}, debuginfo=:none)
-    .section    __TEXT,__text,regular,pure_instructions
+    .text
     movq    (%rsi), %rax
     andq    (%rdi), %rax
     popcntq %rax, %rax
--- src/SmallBitSet.jl
+++ src/SmallBitSet.jl
@@ -3,113 +3,101 @@ module SmallBitSet
 struct Unsafe end
 const unsafe = Unsafe()
 
-struct StackBitSet{M} <: AbstractSet{Int}
+struct StackSet{M} <: AbstractSet{Int}
     x::UInt
 
-    function StackBitSet{M}(x::UInt, ::Unsafe) where M
-        M isa Int || throw(TypeError(:StackBitSet, "", Int, typeof(M)))
+    function StackSet{M}(x::UInt, ::Unsafe) where M
+        M isa Int || throw(TypeError(:StackSet, "", Int, typeof(M)))
         ((M ≤ Sys.WORD_SIZE) & (M > 0)) || throw(DomainError(M, "M must be 1:$(Sys.WORD_SIZE)"))
         new(x)
     end
 end
 
-mask(M) = UInt(1) << M - 1
-StackBitSet{M}() where M = StackBitSet{M}(zero(UInt), unsafe)
-StackBitSet() = StackBitSet{Sys.WORD_SIZE}(zero(UInt), unsafe)
-
-@noinline function throw_StackBitSet_digit_err(::Val{M}) where M
-    throw(ArgumentError("StackBitSet{$(M)} can only contain 0:$(M-1)"))
+function StackSet{M}(v::UInt) where M
+    v ≥ Sys.WORD_SIZE ? throw_StackSet_digit_err(Val(M)) : StackSet{M}(v, unsafe)
 end
 
-function StackBitSet{M}(v::UInt) where M
-    v ≥ Sys.WORD_SIZE && throw_StackBitSet_digit_err(Val(M))
-    StackBitSet{M}(v, unsafe)
-end
-
-function Base.convert(::Type{StackBitSet{M1}}, x::StackBitSet{M2}) where {M1, M2}
-    if (M1 < M2) & (x.x & mask(M1) != x.x)
-        throw_StackBitSet_digit_err(Val(M1))
-    else
-        return StackBitSet{M1}(x.x, unsafe)
-    end
-end
-
-function StackBitSet{M}(itr) where M
-    d = StackBitSet{M}()
+StackSet{M}() where M = StackSet{M}(zero(UInt), unsafe)
+StackSet(x...) = StackSet{Sys.WORD_SIZE}(x...)
+function StackSet{M}(itr) where M
+    d = StackSet{M}()
     for i in itr
         d = push(d, convert(Int, i))
     end
     d
 end
-StackBitSet(itr) = StackBitSet{Sys.WORD_SIZE}(itr)
 
-function Base.promote_rule(::Type{StackBitSet{M1}}, ::Type{StackBitSet{M2}}) where {M1, M2}
-    StackBitSet{max(M1, M2)}
+@noinline function throw_StackSet_digit_err(::Val{M}) where M
+    throw(ArgumentError("StackSet{$(M)} can only contain 0:$(M-1)"))
 end
 
-Base.copy(s::StackBitSet) = s
-Base.empty(x::StackBitSet) = typeof(x)()
-Base.isempty(x::StackBitSet) = iszero(x.x)
-Base.:(==)(x::StackBitSet, y::StackBitSet) = x.x == y.x
-Base.allunique(x::StackBitSet) = true
+function Base.convert(::Type{StackSet{M1}}, x::StackSet{M2}) where {M1, M2}
+    if (M1 < M2) & (x.x & mask(M1) != x.x)
+        throw_StackSet_digit_err(Val(M1))
+    end
+    return StackSet{M1}(x.x, unsafe)
+end
+
+function Base.promote_rule(::Type{StackSet{M1}}, ::Type{StackSet{M2}}) where {M1, M2}
+    StackSet{max(M1, M2)}
+end
+
+mask(M) = UInt(1) << M - 1
+Base.copy(s::StackSet) = s
+Base.empty(x::StackSet) = typeof(x)()
+Base.isempty(x::StackSet) = iszero(x.x)
+Base.:(==)(x::StackSet, y::StackSet) = x.x == y.x
+Base.allunique(x::StackSet) = true
 
 # Julia PR33300 - improved printing of AbstractSets make this obsolete
 @static if VERSION < v"1.4"
-    function Base.show(io::IO, s::StackBitSet{M}) where M
-        print(io, "StackBitSet{$M}([", join(s, ','), "])")
+    function Base.show(io::IO, s::StackSet{M}) where M
+        print(io, "StackSet{$M}([", join(s, ','), "])")
     end
 end
 
-function Base.iterate(x::StackBitSet, state::Int=0)
+function Base.iterate(x::StackSet, state::Int=0)
     bits = x.x >>> unsigned(state)
     iszero(bits) && return nothing
     tz  = trailing_zeros(bits)
-    res = state + tz
-    return (res, res + 1)
+    return (state + tz, state + tz + 1)
 end
 
-Base.length(x::StackBitSet) = count_ones(x.x)
-Base.minimum(x::StackBitSet) = first(x)
-Base.maximum(x::StackBitSet) = last(x)
+Base.length(x::StackSet) = count_ones(x.x)
+Base.minimum(x::StackSet) = first(x)
+Base.maximum(x::StackSet) = last(x)
 
-function Base.last(x::StackBitSet)
+function Base.last(x::StackSet)
     isempty(x) && throw(ArgumentError("collection must be non-empty"))
     Sys.WORD_SIZE - 1 - leading_zeros(x.x)
 end
 
-function Base.in(x::Int, s::StackBitSet{M}) where M
-    u = unsigned(x)
-    (u < M) & isodd(s.x >>> u)
-end
-
-function push(s::StackBitSet, v::Int, ::Unsafe)
+Base.in(x::Int, s::StackSet{M}) where M = (x < M) & isodd(s.x >>> unsigned(x))
+function push(s::StackSet, v::Int, ::Unsafe)
     typeof(s)(s.x | (1 << (unsigned(v) & (Sys.WORD_SIZE - 1))), unsafe)
 end
 
-function push(s::StackBitSet{M}, v::Int) where M
-    unsigned(v) ≥ M && throw_StackBitSet_digit_err(Val(M))
-    push(s, v, unsafe)
+function push(s::StackSet{M}, v::Int) where M
+    unsigned(v) ≥ M ? throw_StackSet_digit_err(Val(M)) : push(s, v, unsafe)
 end
 
-function delete(s::StackBitSet, v::Int)
+pop(s::StackSet, v::Int) = in(s, v) ? delete(s, v) : throw(KeyError(v))
+function delete(s::StackSet, v::Int)
     mask = ~(one(UInt) << unsigned(v))
     typeof(s)(s.x & mask, unsafe)
 end
 
-function pop(s::StackBitSet, v::Int)
-    in(s, v) || throw(KeyError(v))
-    delete(s, v)
-end
-
+Base.issubset(x::StackSet, y::StackSet) = isempty(setdiff(x, y))
+complement(x::StackSet{M}) where M = typeof(x)(x.x ⊻ mask(M), unsafe)
+isdisjoint(x::StackSet, y::StackSet) = isempty(intersect(x, y))
 for (func, op) in ((:union, :|), (:intersect, :&), (:symdiff, :⊻))
     @eval begin
-        function Base.$(func)(x::StackBitSet, y::StackBitSet)
+        function Base.$(func)(x::StackSet, y::StackSet)
             promote_type(typeof(x), typeof(y))($op(x.x, y.x), unsafe)
         end
 
-        Base.$(func)(x, y::StackBitSet) = $func(y, x)
-
-        function Base.$(func)(x::StackBitSet, itr...)
+        Base.$(func)(x, y::StackSet) = $func(y, x)
+        function Base.$(func)(x::StackSet, itr...)
             for i in itr
                 y_ = typeof(x)(i)
                 x = $func(x, y_)
@@ -119,8 +107,8 @@ for (func, op) in ((:union, :|), (:intersect, :&), (:symdiff, :⊻))
     end
 end
 
-Base.setdiff(x::StackBitSet, y::StackBitSet) where M = intersect(x, complement(y))
-function Base.setdiff(x::StackBitSet, itr...)
+Base.setdiff(x::StackSet, y::StackSet) = intersect(x, complement(y))
+function Base.setdiff(x::StackSet, itr...)
     union_ = typeof(x)()
     for i in itr
         y_ = typeof(x)(i)
@@ -129,12 +117,5 @@ function Base.setdiff(x::StackBitSet, itr...)
     return setdiff(x, union_)
 end
 
-Base.issubset(x::StackBitSet, y::StackBitSet) = isempty(setdiff(x, y))
-complement(x::StackBitSet{M}) where M = typeof(x)(x.x ⊻ mask(M), unsafe)
-isdisjoint(x::StackBitSet, y::StackBitSet) = isempty(intersect(x, y))
-
-export StackBitSet,
-       push, delete,
-       complement, isdisjoint
-
+export StackSet, push, delete, complement, isdisjoint
 end # module
